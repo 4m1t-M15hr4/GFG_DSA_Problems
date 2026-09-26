65d5ff06786adb60af87db77eea06bad1c9c6d92
Amit-Mishra <amitmishra31929@gmail.com>
2026-09-26 20:06:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value Equal to Position/value-equal-to-position.py
@@ -0,0 +1,7 @@
+class Solution:
+    def valEqualToPos(self, arr):
+        res = []
+        for i in range(len(arr)):
+            if arr[i] == i +1:
+                res.append(arr[i])
+        return res
